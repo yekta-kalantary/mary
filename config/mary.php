fc319c46dbac932c36280ed66e8ec64cd3c16514
Robson Tenório <rrtenorio@gmail.com>
2023-11-04 23:26:32 -0300

    WIP: Configure component prefix.

--- config/mary.php
+++ config/mary.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    //
+    /**
+     * Default is empty.
+     *    prefix => ''
+     *              <x-button />
+     *              <x-card />
+     *
+     * If you need rename components:
+     *    prefix => 'mary-'
+     *               <x-mary-button />
+     *               <x-mary-card />
+     */
+    'prefix' => ''
 ];
